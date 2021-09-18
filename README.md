4c612d21a689ed49338d7d36a5cf174b2ca91d8f
Сергей Михайлов <s.mihaylov@tionix.ru>
2021-09-18 16:33:14 +0300

    Add requirements, .gitignore. Add README and docstring for filter. Add tests for ansible 2.5-2.9 Add common CI

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # ansible-xml-filters
+
+## Installation
+
+First you need to install requirements:  
+`pip install -r requirements.txt`
+
+You have two options installing filters:
+
+### Copying to your playbook directory (prefered)
+
+By default ansible is searching for `*_plugins` directories in your `playbook_dir`.
+Just copy `filter_plugins` folder to your playbook directory, and you are good to go.
+
+### Using PATH for filter_plugins
+
+Plugin must be placed in directory where ansible is looking for filter_plugins.  
+You can check configured path like this:
+`ansible-config dump|grep DEFAULT_FILTER_PLUGIN_PATH`
+You can either copy plugin to configured directory or modify Ansbile `FILTER_PLUGIN_PATH` via config\env.
+https://docs.ansible.com/ansible/2.9/reference_appendices/config.html#default-filter-plugin-path
+
+## to_pretty_plain_xml
+
+_Get doc:_  
+`python -c 'import re; from filter_plugins import xml; print(re.sub(r"^    ", "", xml.to_pretty_plain_xml.__doc__, flags=re.MULTILINE))'`
--- filter_plugins/xml.py
+++ filter_plugins/xml.py
@@ -1,14 +1,63 @@
-# pylint: disable=too-few-public-methods
+# pylint: disable=too-few-public-methods,missing-module-docstring
 
 import traceback
 import xmlplain
 from ansible import errors
 
 
-def to_pretty_plain_xml(src_obj):
+def to_pretty_plain_xml(src_obj, src_codepage="utf-8"):
+    # pylint: disable=trailing-whitespace
+    """
+    Ansible filter-plugin to format data structure to XML.
+
+    EXAMPLE:
+    ```
+    - hosts: localhost
+      gather_facts: false
+      become: false
+      vars:
+        config_struct:
+          sizes:
+            '@extra': mutable
+            height: '960'
+            weight: '1440'
+      tasks:
+        - debug:
+            msg: "{{ config_struct | to_pretty_plain_xml() }}"
+    ```
+
+    KNOWN ISSUES:
+    * According to XML your should have exact one root-level key.
+    * No numeric data types allowed, nor booleans.  
+      You can use ONLY dicts and strings.  
+      Although you can use lists they are useless in XML:
+        ```
+        # Raw data
+        sizes:
+            - test1
+            - test2
+
+        # XML
+            <?xml version="1.0" encoding="UTF-8"?>
+            <sizes>test1test2</sizes>
+        ```
+    * You can set XML-attributes by prefixing @ to name of a key:
+        ```
+        # Raw data
+        sizes:
+            '@extra': mutable
+            data: some_data
+
+        # XML
+        <?xml version="1.0" encoding="UTF-8"?>
+        <sizes extra="mutable">
+            <data>some_data</data>
+        </sizes>
+        ```
+    """
     plain_xml_str = None
     try:
-        plain_xml_str = xmlplain.xml_from_obj(src_obj).decode("utf-8")
+        plain_xml_str = xmlplain.xml_from_obj(src_obj).decode(src_codepage)
     except Exception as xml_exception:
         exc_msg = "to_pretty_plain_xml(): Cannot convert obj to xml:\n%s" % traceback.format_exc()
         raise errors.AnsibleFilterError(exc_msg) from xml_exception
@@ -16,9 +65,11 @@ def to_pretty_plain_xml(src_obj):
 
 
 class FilterModule():
+    """Ansible interface class"""
     filter_map = {
         'to_pretty_plain_xml': to_pretty_plain_xml
     }
 
     def filters(self):
+        """Ansible interface function"""
         return self.filter_map
